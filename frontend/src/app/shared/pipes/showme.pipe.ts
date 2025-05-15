cff921a853913163e75987c7ef9feb21d9b21ea7
Jeffy6917 <pesgta17@gmail.com>
2025-05-15 10:53:08 -0600

    quitamos showpipe

--- frontend/src/app/shared/pipes/showme.pipe.ts
+++ frontend/src/app/shared/pipes/showme.pipe.ts
@@ -7,7 +7,8 @@ import { API_HOST } from 'src/environments/environment';
 export class ShowmePipe implements PipeTransform {
 
   transform(url: string, ...args: unknown[]): boolean {
-    // if ( API_HOST == "http://localhost:4000") { return true; } 
+    // if ( API_HOST == "http://localhost:4000") { return true; }
+    return true; 
     const accesos = JSON.parse(localStorage.getItem('accesos') || '');
     if ( accesos && accesos.indexOf(url) > -1 ) {
       return true;
